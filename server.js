2dc46e0e16817998d475b80e955487d8c3ef389b
相信我 <15128872145@163.com>
2026-03-10 19:31:02 +0800

    修改跨域

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ ossClient.init();
 const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://niumashuai.top',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
